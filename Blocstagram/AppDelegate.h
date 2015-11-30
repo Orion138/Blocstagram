085e28c17386f424fdf247982695ed11ccd74c00
Orion138 <nathan.boosinger@gmail.com>
2015-11-30 10:37:06 -0600

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Nathan Boosinger on 11/30/15.
+//  Copyright © 2015 Nathan Boosinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
